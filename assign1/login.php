bf37e509d21e9fd73fc4721f31c87a10fd3e7064
timmyhii <timmyhii69@gmail.com>
2025-06-07 16:24:49 +0800

    s

--- assign1/login.php
+++ assign1/login.php
@@ -20,7 +20,7 @@
     </header>
 
     <main class="no-margin-top">
-        
+
         <?php
         if (isset($_GET['error'])) {
             if ($_GET['error'] === 'invalid_credentials') {
@@ -37,7 +37,7 @@
                 <p>Login to Brew & Go. Coffee</p>
             </div>
             <div class="login-right">
-                <form action="admin_login.php" method="post">
+                <form action="login_process.php" method="post">
                     <fieldset>
                         <input class="responsive-hover" type="text" name="username" placeholder="Username" required
                             maxlength="10" pattern="[A-Za-z]+"
