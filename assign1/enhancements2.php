0ea4df17048f3d705d25c65f52b60717993656c2
HaHaBlah <patricksim09@hotmail.com>
2025-06-11 15:18:22 +0800

    .

--- assign1/enhancements2.php
+++ assign1/enhancements2.php
@@ -403,9 +403,9 @@
         <!-- Current Date Feature -->
         <details id="current-date-feature">
             <summary>
-                <h2>Current Date Feature</h2>
+                <h2>Current Year Feature</h2>
             </summary>
-            <p>This feature displays the current date on the website.</p>
+            <p>This feature displays the current year on the website.</p>
             <p>Uses: <a href="footer.inc">footer.inc</a></p>
             <img src="images/enhancements/Current_Date.png" alt="Current Date">
             <h2>footer.inc</h2>
